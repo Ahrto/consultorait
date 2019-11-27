0e9698de025ef9dc6a7c588d6e35609e9cbe9c6b
alberto hernandez <albertohernanrodri@gmail.com>
2019-11-27 18:44:10 +0100

    primer commit

---
+++ docs/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	console.log("todo esta listo");
+
+	
+	
+	$("#candidatos").click(function(){
+		$("#candidatosDesplegable").toggle('medium');
+	});
+
+	$("#blog").click(function(){
+		$("#blogDesplegable").toggle('medium');
+	});
+
+	
+});
